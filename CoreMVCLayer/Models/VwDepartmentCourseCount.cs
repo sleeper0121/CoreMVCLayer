e22a5b4a1c340376672d3714f49c7f0961528738
sleeper0121 <s0934471@gmail.com>
2021-10-03 10:30:52 +0800

    透過 NuGet 指令建出 Model

---
+++ CoreMVCLayer/Models/VwDepartmentCourseCount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CoreMVCLayer.Models
+{
+    public partial class VwDepartmentCourseCount
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public int? CourseCount { get; set; }
+    }
+}
