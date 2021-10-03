4deec135b5eb07a41571ed1e519a3947e92272bf
sleeper0121 <s0934471@gmail.com>
2021-10-03 20:39:52 +0800

    加入 Respository Layer

---
+++ Repository.EF/DepartmentRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Repository.EF
+{
+    public class DepartmentRepository : IRepository<Department>
+    {
+        public readonly ContosoUniversityContext _dbContext;
+
+        public DepartmentRepository(ContosoUniversityContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(Department entity)
+        {
+            _dbContext.Departments.Add(entity);
+            _dbContext.SaveChanges();
+        }
+        public void Update(Department entity)
+        {
+            _dbContext.Departments.Update(entity);
+            _dbContext.SaveChanges();
+        }
+        public void Delete(Department entity)
+        {
+            _dbContext.Departments.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public Department Find(Expression<Func<Department, bool>> where)
+        {
+            return _dbContext.Departments.Where(where).FirstOrDefault();
+        }
+        public IEnumerable<Department> FindAll(Expression<Func<Department, bool>> where)
+        {
+            return _dbContext.Departments.Where(where);
+        }
+
+        public Department Get(long id)
+        {
+            return _dbContext.Departments.SingleOrDefault(b => b.DepartmentId == id);
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _dbContext.Departments.Include(c => c.Instructor).ToList();
+        }
+    }
+}
---
+++ Repository.EF/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Repository.EF
+{
+    public interface IRepository<TEntity>
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(long id);
+
+        TEntity Find(Expression<Func<TEntity, bool>> where);
+
+        IEnumerable<TEntity> FindAll(Expression<Func<TEntity, bool>> where);
+
+        void Add(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+    }
+}
